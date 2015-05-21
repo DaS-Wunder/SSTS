fd13069499dccef80c84c6df730d1927dfb335d9
DaS-Wunder <david.sander@web.de>
2015-05-21 11:18:53 +0200

    Start to create the objects for the tournament

---
+++ src/player.go
@@ -0,0 +1,10 @@
+type Club struct{
+	name string
+	id int
+}
+
+type Player struct{
+	lastName string
+	firstName string
+	club Club
+}
---
+++ src/tournament.go
@@ -0,0 +1,14 @@
+
+import {
+	"player.go"
+	"time"
+}
+
+type Tournament struct{
+	name string
+	date time.Date
+	playerCount int
+	player []Player
+	
+
+}
